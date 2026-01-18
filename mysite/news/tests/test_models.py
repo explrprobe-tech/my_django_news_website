0cc1c4e1ba528e6f6fe4fdb35c307916d87a9bdf
explrprobe <explrprobe@gmail.com>
2026-01-18 13:58:35 +0700

    big tests refactoring

--- mysite/news/tests/test_models.py
+++ mysite/news/tests/test_models.py
@@ -1,141 +1,40 @@
 from django.test import TestCase
 from news.models import News, Category
-from django.contrib.auth.models import User
 from django.urls import reverse
 
 class NewsModelTest(TestCase):
     """Тесты для модели News"""
-    
-    def test_news_creation(self):
-        # 1. Create Category and News for this category
-        category = Category.objects.create(title="Technology")
-        news = News.objects.create(
-            title="First test news",
-            content="Test content for first news",
-            category=category
-        )
-        
-        # 2. Check that category and news were created correct
-        self.assertEqual(news.title, "First test news")
-        self.assertEqual(category.title, "Technology")
-        self.assertEqual(news.category.title, "Technology")
-    
-    def test_news_str_method(self):
-        """Тест: правильно ли отображается новость?""" #TODO BY MYSELF
-        # Здесь будет второй тест
-        pass
-
-class NewsViewsTest(TestCase):
-    """Тесты для представлений"""
-
     def setUp(self):
-        self.user = User.objects.create(username='testuser',
-                                        password='testpass123')
         self.category = Category.objects.create(title="Science")
-
-        self.title_published = "Published News"
-        self.content_published = "Content for the First news"
-        
-        self.title_unpublished = "Unpublished News"
-        self.content_unpublished = "Content for the Second news"
-
         self.news_published = News.objects.create(
-            title = self.title_published,
-            content = self.content_published,
+            title = "Published News",
+            content = "Content for the First news",
             category = self.category,
             is_published = True
         )
-        self.news_unpublished = News.objects.create(
-            title = self.title_unpublished,
-            content = self.content_unpublished,
-            category = self.category,
-            is_published = False
-        )
-
-    def test_news_belongs_to_category(self):
-        """Test: news belongs to category"""
-        self.assertIn(self.news_published, self.category.news_set.all(), 'Published News doesnt belong to category')
-        self.assertIn(self.news_unpublished, self.category.news_set.all(), 'Unpublished News doesnt belong to category')
-
     def test_news_creation(self):
         """Test: news were created properly"""
-
-        self.assertEqual(self.news_published.title, self.title_published, "Published News should have title.")
-        self.assertEqual(self.news_published.content, self.content_published, "Published News should have content.")
-
-        self.assertEqual(self.news_unpublished.title, self.title_unpublished, "Unpublished News should have title.")
-        self.assertEqual(self.news_unpublished.content, self.content_unpublished, "Unpublished News should have content.")
-
+        self.assertIsNotNone(self.news_published.title)
+        self.assertIsNotNone(self.news_published.content)
     def test_news_str_method(self):
         """Test: news displays properly"""
 
         self.assertEqual(str(self.news_published), self.news_published.title)
-        self.assertEqual(str(self.news_unpublished), self.news_unpublished.title)
-
+    def test_news_belongs_to_category(self):
+        """Test: news belongs to category"""
+        self.assertIn(self.news_published, self.category.news_set.all(), 'Published News doesnt belong to category')
     def test_news_get_absolute_url(self):  
         """Test: news can return absolute url"""
-
         published_url = self.news_published.get_absolute_url()
-
         self.assertTrue(published_url.startswith('/'), 'Published news url should start with /')
-
         expected_url = reverse('view_news', kwargs={'pk': self.news_published.pk})
-
-        self.assertEqual(published_url, expected_url, 'Published news url does not match expected url')
-
+        self.assertEqual(published_url, expected_url, 'Published news url does not match expected url')    
     def test_category_get_absolute_url(self):
         """Test: category can return absolute url"""
-
         category_url = self.category.get_absolute_url()
-
         self.assertTrue(category_url.startswith('/'), 'Category url should start with /')
-
         expected_url = reverse(viewname="category", kwargs={'category_id': self.category.pk})
-
         self.assertEqual(category_url, expected_url, 'Category url do not equal to expected')
-
     def test_category_str_method(self):
         """Test: category displays properly"""
-
-        self.assertEqual(str(self.category), self.category.title)
-
-    def test_home_news_page_status_code(self):
-        """Тест: главная страница загружается"""
-
-        response = self.client.get(reverse('news_list'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_home_news_page_shows_news_published(self): 
-        """Тест: публикованная новость отображается"""
-
-        response = self.client.get(reverse('news_list'))
-
-        self.assertEqual(response.status_code, 200)
-
-        self.assertIn('news', response.context, 'В контексте должен быть ключ news_list')
-
-        #получаем query_set из контекста
-        news_list = response.context['news']
-
-        #преобразуем в список для удобства проверки
-        news_items = list(news_list)
-
-        self.assertEqual(len(news_items), 1, f'Должна быть 1 новость, сейчас: {len(news_items)}')
-
-        #вытаскием первую новость из списка
-        news_in_context = news_items[0]
-
-        self.assertEqual(news_in_context.id, self.news_published.id, f'В контексте должна быть новосить с id: {self.news_published.id}')
-
-        self.assertTrue(news_in_context.is_published, "Новость в контекст должна быть опубликованной")
-
-        #преобразуем в список id для удобства проверок
-        news_ids_in_context = [news.id for news in news_items]
-        self.assertNotIn(self.news_unpublished.id, news_ids_in_context, f'Новосить с id: {self.news_unpublished.id} не должна быть в контексте')
-
-    def test_home_news_page_template(self):
-        """Тест: главная страница использует правильный шаблон"""
-
-        response = self.client.get(reverse('news_list'))
-
-        self.assertTemplateUsed(response, 'news/home_news_list.html')
+        self.assertEqual(str(self.category), self.category.title) 
--- mysite/news/tests/test_views.py
+++ mysite/news/tests/test_views.py
@@ -1,8 +1,49 @@
 from django.test import TestCase
 from news.models import News, Category
+from django.urls import reverse
 
-class ViewsTest(TestCase):
+class NewsViewsTest(TestCase):
     "Tests for views in app News"
-    def test_views_creation(self):
-        "Test creation view"
-        pass
+    def setUp(self):
+        self.category = Category.objects.create(title="Science")
+        self.news_published = News.objects.create(
+            title = "Published News",
+            content = "Content for the First news",
+            category = self.category,
+            is_published = True
+        )
+        self.news_unpublished = News.objects.create(
+            title = "Unpublished News",
+            content = "Content for the Second news",
+            category = self.category,
+            is_published = False
+        )
+    def test_home_news_page_status_code(self):
+        """Тест: главная страница загружается"""
+        response = self.client.get(reverse('news_list'))
+        self.assertEqual(response.status_code, 200)
+    def test_home_news_page_shows_news_published(self): 
+        """Тест: публикованная новость отображается"""
+        response = self.client.get(reverse('news_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('news', response.context, 'В контексте должен быть ключ news_list')
+        #получаем query_set из контекста
+        news_list = response.context['news']
+        #преобразуем в список для удобства проверки
+        news_items = list(news_list)
+        self.assertEqual(len(news_items), 1, f'Должна быть 1 новость, сейчас: {len(news_items)}')
+        #вытаскием первую новость из списка
+        news_in_context = news_items[0]
+        self.assertEqual(news_in_context.id, self.news_published.id, f'В контексте должна быть новосить с id: {self.news_published.id}')
+        self.assertTrue(news_in_context.is_published, "Новость в контекст должна быть опубликованной")
+        #преобразуем в список id для удобства проверок
+        news_ids_in_context = [news.id for news in news_items]
+        self.assertNotIn(self.news_unpublished.id, news_ids_in_context, f'Новосить с id: {self.news_unpublished.id} не должна быть в контексте')
+    def test_home_news_page_template(self):
+        """Тест: главная страница использует правильный шаблон"""
+        response = self.client.get(reverse('news_list'))
+        self.assertTemplateUsed(response, 'news/home_news_list.html')
+    def test_news_detail_views_published(self): #todo
+        """Test: News can be taken"""
+        response_news_published = self.client.get(self.news_published.get_absolute_url())
+        self.assertEqual(response_news_published.status_code, 200, 'Published news status code should be 200')
