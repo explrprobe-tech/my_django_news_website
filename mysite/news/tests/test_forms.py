e4ded7bddcec8d018ec9f64c7445ba7bf7513851
explrprobe <explrprobe@gmail.com>
2026-01-26 20:40:01 +0700

    added test_valid_news_form

--- mysite/news/tests/test_forms.py
+++ mysite/news/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from news.models import News, Category
+from news.forms import NewsForm
 
 class NewsFormTest(TestCase):
     "Tests for forms in app News"
@@ -7,13 +8,19 @@ class NewsFormTest(TestCase):
         self.category_science = Category.objects.create(title="Science")
         self.category_biology = Category.objects.create(title="Biology")
         self.form_data = {
-            'title': 'Title for News',
-            'content': 'Content for News',
+            'title': 'Science',
+            'content': 'Content for Science News',
             'category': self.category_science.id
         }
     def test_valid_news_form(self):
         "Test: form works with valid data"
-        pass
+        form =  NewsForm(data=self.form_data)
+        self.assertTrue(form.is_valid())
+        news = form.save()
+        self.assertIsInstance(news, News, 'News object by form is not an instance of News class')
+        self.assertEqual(news.title, 'Science', 'News object by form has not title')
+        self.assertEqual(news.content, 'Content for Science News', 'News object by form has not content')
+        self.assertEqual(news.category, self.category_science.id, 'News object by form has not category')
     def test_news_form_missing_required_fields(self):
         "Test: form show errors for missing required fields"
         pass
