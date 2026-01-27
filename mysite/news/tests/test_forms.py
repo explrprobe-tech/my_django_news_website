f9459fe0639e5a04c0c4c3a6f739b3308ea46032
explrprobe <explrprobe@gmail.com>
2026-01-27 21:37:55 +0700

    added test_news_form_missing_required_fields

--- mysite/news/tests/test_forms.py
+++ mysite/news/tests/test_forms.py
@@ -16,7 +16,7 @@ class NewsFormTest(TestCase):
     def test_valid_news_form(self):
         "Test: form works with valid data"
         form =  NewsForm(data=self.form_data)
-        self.assertTrue(form.is_valid())
+        self.assertTrue(form.is_valid(), 'Form is not valid')
         news = form.save()
         self.assertIsInstance(news, News, 'News object by form is not an instance of News class')
         self.assertEqual(news.title, 'Science', 'News object by form has not title')
@@ -24,8 +24,21 @@ class NewsFormTest(TestCase):
         self.assertEqual(news.category, self.category_science.id, 'News object by form has not category')
         self.assertTrue(news.is_published, 'News object by form does not have attribute is_published')
     def test_news_form_missing_required_fields(self):
-        "Test: form show errors for missing required fields"
-        pass
+        "Test: form shows errors only for missing required fields"
+        required_fields = ['title', 'category']
+        for field in required_fields:
+            form_data_copy = self.form_data.copy()
+            del form_data_copy[field]
+            form = NewsForm(data=form_data_copy)
+            self.assertFalse(form.is_valid(), f'{field} should be required')
+            self.assertIn(field, form.errors, f'Form does not show error for required field - {field}')
+        not_required_fields = ['content', 'is_published']
+        for field in not_required_fields:
+            form_data_copy = self.form_data.copy()
+            del form_data_copy[field]
+            form = NewsForm(data=form_data_copy)
+            self.assertTrue(form.is_valid(), f'{field} should not be required')
+            self.assertNotIn(field, form.errors, f'Form does show error for not required field - {field}')
     def test_news_form_invalid_data(self):
         "Test: form validation with invalid data"
         pass
