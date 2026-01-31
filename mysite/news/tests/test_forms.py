ac06603cf5fd5c9dd01c9b08c09368fe50a606d6
explrprobe <explrprobe@gmail.com>
2026-01-31 16:29:57 +0700

    added test_register_form_missing_required_fields + refactoring another form tests

--- mysite/news/tests/test_forms.py
+++ mysite/news/tests/test_forms.py
@@ -7,58 +7,58 @@ class NewsFormTest(TestCase):
     def setUp(self):
         self.category_science = Category.objects.create(title="Science")
         self.category_biology = Category.objects.create(title="Biology")
-        self.form_data = {
+        self.news_form_data = {
             'title': 'Science',
             'content': 'Content for Science News',
             'category': self.category_science.id,
             'is_published': True
         }
-    def test_news_form_valid(self):
-        "Test: form works with valid data"
-        form =  NewsForm(data=self.form_data)
-        self.assertTrue(form.is_valid(), 'Form is not valid')
-        news = form.save()
-        self.assertIsInstance(news, News, 'News object by form is not an instance of News class')
-        self.assertEqual(news.title, 'Science', 'News object by form has not title')
-        self.assertEqual(news.content, 'Content for Science News', 'News object by form has not content')
-        self.assertEqual(news.category, self.category_science.id, 'News object by form has not category')
-        self.assertTrue(news.is_published, 'News object by form does not have attribute is_published')
-    def test_news_form_missing_required_fields(self):
-        "Test: form shows errors only for missing required fields"
-        required_fields = ['title', 'category']
-        for field in required_fields:
-            form_data_copy = self.form_data.copy()
-            del form_data_copy[field]
-            form = NewsForm(data=form_data_copy)
-            self.assertFalse(form.is_valid(), f'{field} should be required')
-            self.assertIn(field, form.errors, f'Form does not show error for required field - {field}')
-        not_required_fields = ['content', 'is_published']
-        for field in not_required_fields:
-            form_data_copy = self.form_data.copy()
-            del form_data_copy[field]
-            form = NewsForm(data=form_data_copy)
-            self.assertTrue(form.is_valid(), f'{field} should not be required')
-            self.assertNotIn(field, form.errors, f'Form does show error for not required field - {field}')
-    def test_news_form_invalid_data(self):
-        "Test: form validation with invalid data"
-        form_data_invalid = {
-            'title': '123 Invalid title',
-            'content': 'Content for invalid form',
-            'category': self.category_biology.id,
-            'is_published': True
-        }
-        news_invalid = NewsForm(data=form_data_invalid)
-        self.assertFalse(news_invalid.is_valid(), 'News object by invalid form should be invalid')
-        self.assertIn('title', news_invalid.errors, 'Title with start by number should be invalid')
-    def test_register_form_valid(self):
-        "Test: register form works with valid data"
-        form_data = {
+        self.register_form_data = {
             'username': 'Test_user_form',
             'email': 'Test_user_form@mail.ru',
             'password1': 'Coplexpassword123!',
             'password2': 'Coplexpassword123!'
         }
-        register = RegisterForm(data=form_data)
+    def test_news_form_valid(self):
+        "Test: form works with valid data"
+        form =  NewsForm(data=self.news_form_data)
+        self.assertTrue(form.is_valid(), 'Form is not valid')
+        news = form.save()
+        self.assertIsInstance(news, News, 'News object by form is not an instance of News class')
+        self.assertEqual(news.title, 'Science', 'News object by form has not title')
+        self.assertEqual(news.content, 'Content for Science News', 'News object by form has not content')
+        self.assertEqual(news.category.id, self.category_science.id, 'News object by form has not category')
+        self.assertTrue(news.is_published, 'News object by form does not have attribute is_published')
+    def test_news_form_missing_required_fields(self):
+        "Test: form shows errors only for missing required fields"
+        required_fields = ['title', 'category']
+        for field in required_fields:
+            form_data_copy = self.news_form_data.copy()
+            del form_data_copy[field]
+            news_valid = NewsForm(data=form_data_copy)
+            self.assertFalse(news_valid.is_valid(), f'{field} should be required')
+            self.assertIn(field, news_valid.errors, f'News form does not show error for required field - {field}')
+        not_required_fields = ['content', 'is_published']
+        for field in not_required_fields:
+            form_data_copy = self.news_form_data.copy()
+            del form_data_copy[field]
+            news_invalid = NewsForm(data=form_data_copy)
+            self.assertTrue(news_invalid.is_valid(), f'{field} should not be required')
+            self.assertNotIn(field, news_invalid.errors, f'News form does show error for not required field - {field}')
+    def test_news_form_invalid_data(self):
+        "Test: form validation with invalid data"
+        news_form_data_invalid = {
+            'title': '123 Invalid title',
+            'content': 'Content for invalid form',
+            'category': self.category_biology.id,
+            'is_published': True
+        }
+        news_invalid = NewsForm(data=news_form_data_invalid)
+        self.assertFalse(news_invalid.is_valid(), 'News object by invalid form should be invalid')
+        self.assertIn('title', news_invalid.errors, 'Title with start by number should be invalid')
+    def test_register_form_valid(self):
+        "Test: register form works with valid data"
+        register = RegisterForm(data=self.register_form_data)
         self.assertTrue(register.is_valid(), f'Register form should be valid. Error: {register.errors if not register.is_valid() else None}')
         user_test = register.save()
         self.assertEqual(user_test.username, 'Test_user_form')
@@ -66,6 +66,13 @@ class NewsFormTest(TestCase):
         self.assertTrue(user_test.check_password('Coplexpassword123!'), 'user test password should match')
         self.assertTrue(user_test.is_active, 'user test should be active')
     def test_register_form_missing_required_fields(self):
-        pass
+        "Test: register form doesn't work with invalid data"
+        required_fields = ['username', 'email', 'password1', 'password2']
+        for field in required_fields:
+            register_form_data_copy = self.register_form_data.copy()
+            del register_form_data_copy[field]
+            register_valid = RegisterForm(data=register_form_data_copy)
+            self.assertFalse(register_valid.is_valid(), f'{field} should be required')
+            self.assertIn(field, register_valid.errors, f'Register form does not show error for {field}')
     def test_register_form_invalid_data(self):
         pass
