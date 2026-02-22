903b86152e4901c877ddf918a62017c4c7bd7514
explrprobe <explrprobe@gmail.com>
2026-02-22 11:37:51 +0700

    added category_news_list.html, fixed context_object_name in NewsByCategory. Also added pictures in database for news

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -114,8 +114,8 @@ class HomeNews(ListView):
     
 class NewsByCategory(ListView):
     model = News
-    template_name = 'news/home_news_list.html'
-    context_object_name = 'news'
+    template_name = 'news/category_news_list.html'
+    context_object_name = 'latest_news'
     allow_empty = False 
     
     def get_context_data(self, *, object_list=None, **kwargs):
