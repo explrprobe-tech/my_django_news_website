2793c8f8bb28f66e6dc97608e0bb08d641c1c002
explrprobe <explrprobe@gmail.com>
2026-05-11 16:57:25 +0700

    changed rules for editor for edition news on news_edit page

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -150,6 +150,7 @@ class HomeNews(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Home Page'
+        context['can_edit_news'] = is_editor_or_admin(self.request.user)
         return context
     
     def get_queryset(self):
